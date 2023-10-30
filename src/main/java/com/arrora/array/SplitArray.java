369627b204fb0e75c9c05e2a50a1a2918fbfd5ed
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-30 18:15:27 +0530

    git rebase origin/main

--- src/main/java/com/arrora/array/SplitArray.java
+++ src/main/java/com/arrora/array/SplitArray.java
@@ -2,10 +2,12 @@ package com.arrora.array;
 
 public class SplitArray 
 {
-
+	static void splitArray(int[] arr)
+	{
+		
+	}
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 
 	}
 
