47a93a3ddc98b83ee04f0485ca02773575ac8537
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-30 17:53:09 +0530

    initial commit

---
+++ src/main/java/com/arrora/ArroraApplication.java
@@ -0,0 +1,15 @@
+package com.arrora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArroraApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(ArroraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arrora/ArroraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
