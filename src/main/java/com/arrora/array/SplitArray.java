e998ebb68103dfba8499a190ec6b29f52fb6aa09
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-30 18:36:31 +0530

    length is an property of Array

--- src/main/java/com/arrora/array/SplitArray.java
+++ src/main/java/com/arrora/array/SplitArray.java
@@ -4,7 +4,7 @@ public class SplitArray
 {
 	static void splitArray(int[] arr)
 	{
-		
+		System.out.println("\n Original size of the array:"+arr.length);
 	}
 	public static void main(String[] args)
 	{
