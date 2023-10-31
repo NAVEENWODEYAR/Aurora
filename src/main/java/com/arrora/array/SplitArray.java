fd6a512a3318a3e1c5ed6f35dc239fe135f47975
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-31 08:14:54 +0530

    Arrays.copyOfRange(arr, 0, 3);

--- src/main/java/com/arrora/array/SplitArray.java
+++ src/main/java/com/arrora/array/SplitArray.java
@@ -1,10 +1,13 @@
 package com.arrora.array;
 
+import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.pbkdf2.Arrays;
+
 public class SplitArray 
 {
 	static void splitArray(int[] arr)
 	{
 		System.out.println("\n Original size of the array:"+arr.length);
+		int[] subArray1 = Arrays.copyOfRange(arr, 0, 3);
 	}
 	public static void main(String[] args)
 	{
