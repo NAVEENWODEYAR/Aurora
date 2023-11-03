33d8d40cd4f3506215e808535406f0125fc00d5f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-03 16:29:12 +0530

    GE

--- src/main/java/com/arrora/ArroraApplication.java
+++ src/main/java/com/arrora/ArroraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArroraApplication 
+public class ArroraApplication
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		SpringApplication.run(ArroraApplication.class, args);
 		System.out.println("\n Roaring begins");
--- src/main/java/com/arrora/array/SplitArray.java
+++ src/main/java/com/arrora/array/SplitArray.java
@@ -2,7 +2,7 @@ package com.arrora.array;
 
 import java.util.Arrays;
 
-public class SplitArray 
+public class SplitArray
 {
 	// subarray from original array..
 	static void splitArray(int[] arr)
@@ -10,7 +10,7 @@ public class SplitArray
 		System.out.println("\n Original size of the array:"+arr.length);
 		int[] subArray = Arrays.copyOfRange(arr, 0, 3);
 	}
-	
+
 	public static void main(String[] args)
 	{
 		splitArray(new int[] {1,2,3,4,5,6,7,8,9});
