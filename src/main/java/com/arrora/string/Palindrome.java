0ba5403c163a8c971c474993b1557cd682a6e043
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-12 22:45:30 +0530

    String is an Immutable class,

---
+++ src/main/java/com/arrora/string/Palindrome.java
@@ -0,0 +1,16 @@
+package com.arrora.string;
+
+public class Palindrome 
+{
+	
+	static void testPalindrome(String st)
+	{
+		System.out.println("\n Original String given,"+st);
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("\n Palindrome Program,");
+	}
+
+}
