91748ecdcb7de3ab649a2bcd447e0d259aebddd0
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-13 22:23:28 +0530

    for(;;)

--- src/main/java/com/arrora/string/Palindrome.java
+++ src/main/java/com/arrora/string/Palindrome.java
@@ -6,6 +6,10 @@ public class Palindrome
 	static void testPalindrome(String st)
 	{
 		System.out.println("\n Original String given,"+st);
+			for(;;)
+			{
+				// infinate loop.,
+			}
 	}
 	
 	public static void main(String[] args) 
